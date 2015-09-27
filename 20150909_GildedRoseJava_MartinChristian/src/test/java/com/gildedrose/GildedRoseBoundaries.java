8d876c742609ecddf3a149bfac821e98f17582a6
masterhilli <martin.hillbrand@gmail.com>
2015-09-27 19:56:53 +0200

    added parameterized test for quality of gilded rose tests

--- 20150909_GildedRoseJava_MartinChristian/src/test/java/com/gildedrose/GildedRoseBoundaries.java
+++ 20150909_GildedRoseJava_MartinChristian/src/test/java/com/gildedrose/GildedRoseBoundaries.java
@@ -5,9 +5,10 @@ public abstract class GildedRoseBoundaries {
 
 	protected static final int MAXIMUM_QUALITY = 50;
 	protected static final int MINIMUM_QUALITY = 0;
+	protected static final int ON_SELL_IN_DAY = 0;
 	protected static final int PAST_SELLIN_DAYS = -1;
 	protected static final int EXAMPLE_SELLINDAY = 22;
-	protected static final int EXAMPLE_QUALITY = 10;
+	protected static final int EXAMPLE_QUALITY = 22;
 	protected static final String NAME_ORDINARY_ITEM = "Ordinary Item";
 	protected static final String NAME_AGED_BRIE = "Aged Brie";
 	protected static final String NAME_SULFURAS = "Sulfuras, Hand of Ragnaros";
--- 20150909_GildedRoseJava_MartinChristian/src/test/java/com/gildedrose/GildedRoseParameterizedQualityTest.java
+++ 20150909_GildedRoseJava_MartinChristian/src/test/java/com/gildedrose/GildedRoseParameterizedQualityTest.java
@@ -1,30 +1,118 @@
 package com.gildedrose;
 
-public class GildedRoseParameterizedQualityTest {
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class GildedRoseParameterizedQualityTest extends GildedRoseBasicTestUsage {
+	@Parameters
+	public static Collection<Object[]> parameters() {
+		return Arrays.asList(new Object[][] {
+				{ GildedRoseBoundaries.NAME_ORDINARY_ITEM, GildedRoseBoundaries.EXAMPLE_SELLINDAY, 2, 1 },
+				{ GildedRoseBoundaries.NAME_ORDINARY_ITEM, GildedRoseBoundaries.EXAMPLE_SELLINDAY, GildedRoseBoundaries.MINIMUM_QUALITY, GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_ORDINARY_ITEM, 1, GildedRoseBoundaries.EXAMPLE_QUALITY, 21 },
+				{ GildedRoseBoundaries.NAME_ORDINARY_ITEM, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.EXAMPLE_QUALITY, 20 },
+				{ GildedRoseBoundaries.NAME_ORDINARY_ITEM, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.MINIMUM_QUALITY, GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_ORDINARY_ITEM, GildedRoseBoundaries.PAST_SELLIN_DAYS, 1,
+						GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_ORDINARY_ITEM, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.EXAMPLE_QUALITY, 20 },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.EXAMPLE_QUALITY, 24 },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.MINIMUM_QUALITY, 2 },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY, GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY - 1, GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.EXAMPLE_SELLINDAY,
+						GildedRoseBoundaries.EXAMPLE_QUALITY, 23 },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.EXAMPLE_SELLINDAY,
+						GildedRoseBoundaries.MINIMUM_QUALITY, 1 },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.EXAMPLE_SELLINDAY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY, GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.EXAMPLE_SELLINDAY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY - 1, GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.MAXIMUM_QUALITY, GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_AGED_BRIE, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.MAXIMUM_QUALITY - 1, GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_SULFURAS, 1, GildedRoseBoundaries.MAXIMUM_QUALITY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_SULFURAS, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY, GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_SULFURAS, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.MAXIMUM_QUALITY, GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_SULFURAS, 1, GildedRoseBoundaries.EXAMPLE_QUALITY,
+						GildedRoseBoundaries.EXAMPLE_QUALITY },
+				{ GildedRoseBoundaries.NAME_SULFURAS, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.EXAMPLE_QUALITY, GildedRoseBoundaries.EXAMPLE_QUALITY },
+				{ GildedRoseBoundaries.NAME_SULFURAS, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.EXAMPLE_QUALITY, GildedRoseBoundaries.EXAMPLE_QUALITY },
+				{ GildedRoseBoundaries.NAME_SULFURAS, 1, GildedRoseBoundaries.MINIMUM_QUALITY,
+						GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_SULFURAS, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.MINIMUM_QUALITY, GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_SULFURAS, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.MINIMUM_QUALITY, GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 11, GildedRoseBoundaries.MINIMUM_QUALITY, 1 },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 11, GildedRoseBoundaries.MAXIMUM_QUALITY - 1,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 11, GildedRoseBoundaries.MAXIMUM_QUALITY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 10, GildedRoseBoundaries.MINIMUM_QUALITY, 2 },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 10, GildedRoseBoundaries.MAXIMUM_QUALITY - 1,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 10, GildedRoseBoundaries.MAXIMUM_QUALITY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 6, GildedRoseBoundaries.MINIMUM_QUALITY, 2 },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 6, GildedRoseBoundaries.MAXIMUM_QUALITY - 1,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 6, GildedRoseBoundaries.MAXIMUM_QUALITY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 5, GildedRoseBoundaries.MINIMUM_QUALITY, 3 },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 5, GildedRoseBoundaries.MAXIMUM_QUALITY - 1,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 5, GildedRoseBoundaries.MAXIMUM_QUALITY - 2,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 5, GildedRoseBoundaries.MAXIMUM_QUALITY - 3,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, 5, GildedRoseBoundaries.MAXIMUM_QUALITY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.MINIMUM_QUALITY, GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY - 1, GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, GildedRoseBoundaries.ON_SELL_IN_DAY,
+						GildedRoseBoundaries.MAXIMUM_QUALITY, GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.MINIMUM_QUALITY, GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.MAXIMUM_QUALITY - 1, GildedRoseBoundaries.MINIMUM_QUALITY },
+				{ GildedRoseBoundaries.NAME_BACKSTAGE_PASS, GildedRoseBoundaries.PAST_SELLIN_DAYS,
+						GildedRoseBoundaries.MAXIMUM_QUALITY, GildedRoseBoundaries.MINIMUM_QUALITY } });
+	};
+
+	private int expectedQuality;
+
+	public GildedRoseParameterizedQualityTest(final String name, final int sellInDay, final int quality,
+			final int expectedQuality) {
+		super();
+		addItem(name, sellInDay, quality);
+		this.expectedQuality = expectedQuality;
+	}
+
+	@Test
+	public void testIfSellInDaysWhereUpdated() {
+		// given Method is already done through constructor
+		whenQualityIsUpdated();
+		thenTheQualityShouldBe(expectedQuality);
+	}
 }
-
-
-//@RunWith(Parameterized.class)
-//public class FibonacciTest {
-//    @Parameters
-//    public static Collection<Object[]> data() {
-//        return Arrays.asList(new Object[][] {     
-//                 { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 },{ 6, 8 }  
-//           });
-//    }
-//
-//    private int fInput;
-//
-//    private int fExpected;
-//
-//    public FibonacciTest(int input, int expected) {
-//        fInput= input;
-//        fExpected= expected;
-//    }
-//
-//    @Test
-//    public void test() {
-//        assertEquals(fExpected, Fibonacci.compute(fInput));
-//    }
-//}
